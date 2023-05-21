ca2cbfda689756058e29dbb9b8797206765ce5dc
Mody105 <134178500+Mody105@users.noreply.github.com>
2023-05-21 03:53:28 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+def no_gasture_shake2(
+ basic.showNumber(randint(1,9))
+input.onGesture(Gesture.Shake, function() {
+    X =0
+    global X
+    X += 1
+
+    basic.showNumber(x)
+    input.onButtonPressed(Button.A, function() {
+
+    
+    }
+}
